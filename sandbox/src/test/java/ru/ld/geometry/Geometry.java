90dfbc63c1ad5282e84d496de3485a7a041a2c12
Lesnyanka <dubik90@mail.ru>
2024-02-10 21:05:27 +0300

    Выполнена структуризация кода

---
+++ sandbox/src/test/java/ru/ld/geometry/Geometry.java
@@ -0,0 +1,21 @@
+package ru.ld.geometry;
+
+import ru.ld.geometry.figures.Rectangle;
+import ru.ld.geometry.figures.Square;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Square.printSquareArea(7.0);
+        Square.printSquareArea(5.0);
+        Square.printSquareArea(3.0);
+
+        Rectangle.printRectanglArea(3.0, 5.0);
+        Rectangle.printRectanglArea(7.0, 9.0);
+
+
+     }
+
+}
+
+
+
---
+++ sandbox/src/test/java/ru/ld/geometry/figures/Rectangle.java
@@ -0,0 +1,12 @@
+package ru.ld.geometry.figures;
+
+public class Rectangle {
+    public static void printRectanglArea(double a, double b) {
+        var text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", a, b, rectanglArea(a, b));
+        System.out.println(text );
+    }
+
+    private static double rectanglArea(double a, double b) {
+        return a * b;
+    }
+}
