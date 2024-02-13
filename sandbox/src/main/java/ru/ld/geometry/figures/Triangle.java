7421274f8be97818625cf6b26dc1ff8046e93b7b
Lesnyanka <dubik90@mail.ru>
2024-02-13 20:56:05 +0300

    Оптимизировано

--- sandbox/src/main/java/ru/ld/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/ld/geometry/figures/Triangle.java
@@ -1,13 +1,13 @@
 package ru.ld.geometry.figures;
 
 public record Triangle(double a, double b, double c) {
- 
+
    public double countTriangle(){
        return this.a + this.b + this.c;
    }
 
     public double squareTriangle() {
-        double p = (a + b + c) / 2;
+        double p = (countTriangle()) / 2;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 }
