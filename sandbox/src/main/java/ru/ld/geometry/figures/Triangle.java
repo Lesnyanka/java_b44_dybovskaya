dec38f8fec205e39b9f1733476023346c50e948d
Lesnyanka <dubik90@mail.ru>
2024-02-17 10:50:38 +0300

    Созданы тесты
    Сторона треугольника не может быть меньше 0; неравенство треугольника

--- sandbox/src/main/java/ru/ld/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/ld/geometry/figures/Triangle.java
@@ -2,12 +2,25 @@ package ru.ld.geometry.figures;
 
 public record Triangle(double a, double b, double c) {
 
-   public double countTriangle(){
+    /*метод расчета периметра треугольника*/
+    public double countTriangle(){
        return this.a + this.b + this.c;
    }
-
+   /*метод расчета площади треугольника*/
     public double squareTriangle() {
         double p = (countTriangle()) / 2;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
+
+    /* 3.1 создания треугольника с отрицательной длиной стороны
+    * 3.2 неравенство треугольника*/
+    public Triangle {
+        if (a < 0 || b < 0 || c < 0){
+            throw new IllegalArgumentException("Triangle side should be non-negative");
+        }
+        else if ((a + b) > c || (a + c) > b || (c + b) > a ){
+            throw new IllegalArgumentException("Violation Triangle inequality");
+        }
+    }
+
 }
--- sandbox/src/test/java/ru/ld/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/ld/geometry/figures/TriangleTests.java
@@ -15,6 +15,34 @@ public class TriangleTests {
         Assertions.assertEquals(81.97560612767678, new Triangle(16.0, 13.0, 13.0).squareTriangle());
 
     }
+/* Нельзя создать треугольник с отрицательной стороной/*
+
+ */
+    @Test
+    void cannotCreateTriangleWithNegativeSide(){
+        try {
+            new Triangle(-5.0, 10.0, 10.0);
+            Assertions.fail();
+
+        }catch(IllegalArgumentException exeption){
+            //ok
+        }
 
 
-}
+
+
+    }
+    /*Сумма двух любых сторон должна быть не меньше третьей стороны*/
+    @Test
+    void violationTringleInequality(){
+        try {
+            new Triangle(5.0, 8.0, 15.0);
+            Assertions.fail();
+        }catch(IllegalArgumentException exeption){
+            //ok
+        }
+        }
+    }
+
+
+
