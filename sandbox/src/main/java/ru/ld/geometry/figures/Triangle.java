fa422c62095d563cfe8084db8781cbfc0541ed83
Lesnyanka <dubik90@mail.ru>
2024-02-21 13:56:02 +0300

    Доработано задание 3
    Исправлено условие для выброса исключения Violation Triangle inequality

--- sandbox/src/main/java/ru/ld/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/ld/geometry/figures/Triangle.java
@@ -5,10 +5,11 @@ import java.util.Objects;
 public record Triangle(double a, double b, double c) {
 
     /*метод расчета периметра треугольника*/
-    public double countTriangle(){
-       return this.a + this.b + this.c;
-   }
-   /*метод расчета площади треугольника*/
+    public double countTriangle() {
+        return this.a + this.b + this.c;
+    }
+
+    /*метод расчета площади треугольника*/
     public double squareTriangle() {
         double p = (countTriangle()) / 2;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
@@ -17,25 +18,20 @@ public record Triangle(double a, double b, double c) {
     /* 3.1 создания треугольника с отрицательной длиной стороны
    3. Нарушается неравенство треугольника
    */
-    public  Triangle(double a, double b, double c) {
+    public Triangle(double a, double b, double c) {
         this.a = a;
         this.b = b;
         this.c = c;
-        if (a < 0 || b < 0 || c < 0) {
+        if (a < 0 || b < 0 || c < 0){
             throw new IllegalArgumentException("Triangle side should be non-negative");
-        } else if ((a + b) > c || (a + c) > b || (c + b) > a ){
+        } else if ((a + b) < c || (a + c) < b || (c + b) < a ){
             throw new IllegalArgumentException("Violation Triangle inequality");
         }
 
-        }
 
 
 
-
-
-
-
-
+    }
 
 
     /*4.1 Сравнение треугольников*/
@@ -45,11 +41,11 @@ public record Triangle(double a, double b, double c) {
         if (o == null || getClass() != o.getClass()) return false;
         Triangle triangle = (Triangle) o;
         return (Double.compare(triangle.a, this.a) == 0 && Double.compare(triangle.b, this.b) == 0 && Double.compare(triangle.c, this.c) == 0)
-                ||(Double.compare(triangle.a, this.b) == 0 && Double.compare(triangle.b, this.a) == 0 && Double.compare(triangle.c, this.c) == 0)
-                ||(Double.compare(triangle.a, this.c) == 0 && Double.compare(triangle.b, this.a) == 0 && Double.compare(triangle.c, this.b) == 0)
-                ||(Double.compare(triangle.a, this.a) == 0 && Double.compare(triangle.b, this.c) == 0 && Double.compare(triangle.c, this.b) == 0)
-                ||(Double.compare(triangle.a, this.b) == 0 && Double.compare(triangle.b, this.c) == 0 && Double.compare(triangle.c, this.a) == 0)
-                ||(Double.compare(triangle.a, this.c) == 0 && Double.compare(triangle.b, this.b) == 0 && Double.compare(triangle.c, this.a) == 0);
+                || (Double.compare(triangle.a, this.b) == 0 && Double.compare(triangle.b, this.a) == 0 && Double.compare(triangle.c, this.c) == 0)
+                || (Double.compare(triangle.a, this.c) == 0 && Double.compare(triangle.b, this.a) == 0 && Double.compare(triangle.c, this.b) == 0)
+                || (Double.compare(triangle.a, this.a) == 0 && Double.compare(triangle.b, this.c) == 0 && Double.compare(triangle.c, this.b) == 0)
+                || (Double.compare(triangle.a, this.b) == 0 && Double.compare(triangle.b, this.c) == 0 && Double.compare(triangle.c, this.a) == 0)
+                || (Double.compare(triangle.a, this.c) == 0 && Double.compare(triangle.b, this.b) == 0 && Double.compare(triangle.c, this.a) == 0);
 
     }
 
--- sandbox/src/test/java/ru/ld/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/ld/geometry/figures/TriangleTests.java
@@ -10,62 +10,59 @@ public class TriangleTests {
         double result = s.countTriangle();
         Assertions.assertEquals(22.0, result);
     }
+
     @Test
-    void calculateSquarePerimetr(){
+    void calculateSquarePerimetr() {
         Assertions.assertEquals(81.97560612767678, new Triangle(16.0, 13.0, 13.0).squareTriangle());
 
     }
 
 
+    /* 3.1 Исключение при попытке создать треугольник с отрицательной длиной стороны,*/
 
-
-
-
-    /* 3.1 исключение при попытке создать треугольник с отрицательной длиной стороны,*/
     @Test
-    void cannotCreativeTriangleWithNegativeSide(){
+    void cannotCreativeTriangleWithNegativeSide() {
         try {
             new Triangle(-5.0, 8.0, 15.0);
             Assertions.fail();
-        }catch(IllegalArgumentException exception){
-            //ok
+        } catch (IllegalArgumentException exception) {
         }
+
     }
 
-    /*Нарушается неравенство треугольника*/
+    /* 3.2 Нарушается неравенство треугольника*/
+
     @Test
-    void violationTriangleInequality(){
+    void violationTriangleInequality() {
         try {
             new Triangle(5.0, 8.0, 15.0);
             Assertions.fail();
-        }catch(IllegalArgumentException exception){
+        } catch (IllegalArgumentException exception) {
             //ok
         }
     }
 
-      /* 4.1 Сравнение треугольников*/
+
+
+
+    /* 4.1 Сравнение треугольников*/
 
     @Test
-
     void compareTriangles() {
-          var tri1 = new Triangle(6.0, 7.0, 10.0);
-          var tri2 = new Triangle(6.0, 10.0, 7.0);
-          var tri3 = new Triangle(7.0, 6.0, 10.0);
-          var tri4 = new Triangle(7.0, 10.0, 6.0);
-          var tri5 = new Triangle(10.0, 7.0, 6.0);
-          var tri6 = new Triangle(10.0, 6.0, 7.0);
-          Assertions.assertEquals(tri1, tri2);
-          Assertions.assertEquals(tri1, tri3);
-          Assertions.assertEquals(tri2, tri3);
-          Assertions.assertEquals(tri4, tri5);
-          Assertions.assertEquals(tri5, tri6);
-          Assertions.assertEquals(tri3, tri6);
+        var tri1 = new Triangle(6.0, 7.0, 10.0);
+        var tri2 = new Triangle(6.0, 10.0, 7.0);
+        var tri3 = new Triangle(7.0, 6.0, 10.0);
+        var tri4 = new Triangle(7.0, 10.0, 6.0);
+        var tri5 = new Triangle(10.0, 7.0, 6.0);
+        var tri6 = new Triangle(10.0, 6.0, 7.0);
+        Assertions.assertEquals(tri1, tri2);
+        Assertions.assertEquals(tri1, tri3);
+        Assertions.assertEquals(tri2, tri3);
+        Assertions.assertEquals(tri4, tri5);
+        Assertions.assertEquals(tri5, tri6);
+        Assertions.assertEquals(tri3, tri6);
 
-      }
-
-
-
-
+    }
 
 
 }
