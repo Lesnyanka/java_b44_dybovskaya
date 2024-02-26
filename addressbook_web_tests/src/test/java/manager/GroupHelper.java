ed5271fbc7ec4ea5f861ac1d140ba09dfae294eb
Lesnyanka <dubik90@mail.ru>
2024-02-26 09:24:25 +0300

    Модификация тестов

---
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -0,0 +1,99 @@
+package manager;
+
+import model.GroupData;
+import org.openqa.selenium.By;
+
+public class GroupHelper {
+    private final ApplicationManager manager;
+
+    public GroupHelper(ApplicationManager manager){
+
+        this.manager = manager;
+    }
+    public void createGroup(GroupData group) {
+        openGroupsPage();
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupsPage();
+    }
+
+    public void removeGroup() {
+        openGroupsPage();
+        selectGroup();
+        removeSelectedGroup();
+        returnToGroupsPage();
+    }
+
+    public void modifyGroup(GroupData modifedGroup) {
+        openGroupsPage();
+        selectGroup();
+        initGroupModification();
+        fillGroupForm(modifedGroup);
+        submitGroupModification();
+        returnToGroupsPage();
+    }
+    public void openGroupsPage() {
+        if (!manager.isElementPresent(By.name("new"))) {
+            click(By.linkText("groups"));
+        }
+    }
+
+    public boolean isGroupPresent() {
+        openGroupsPage();
+        return manager.isElementPresent(By.name("selected[]"));
+    }
+
+
+
+    private void submitGroupCreation() {
+        click(By.name("submit"));
+    }
+
+
+
+    private void initGroupCreation() {
+        click(By.name("new"));
+    }
+
+
+
+    private void removeSelectedGroup() {
+        click(By.name("delete"));
+    }
+
+
+
+    private void returnToGroupsPage() {
+        click(By.linkText("groups"));
+    }
+
+    private void submitGroupModification() {
+        click(By.name("update"));
+
+    }
+
+    private void fillGroupForm(GroupData group) {
+        type(By.name("group_name"), group.name());
+        type(By.name("group_header"), group.header());
+        type(By.name("group_footer"), group.footer());
+    }
+
+    private void type(By locator, String text) {
+        click(locator);
+        manager.driver.findElement(locator).sendKeys(text);
+    }
+
+    private void initGroupModification() {
+        click(By.name("edit"));
+    }
+
+    private void selectGroup() {
+        click(By.name("selected[]"));
+
+    }
+
+    private void click(By locator) {
+        manager.driver.findElement(locator).click();
+    }
+}
